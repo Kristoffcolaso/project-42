a6f0d88adbf163775c962ab154bcfbfd63c2e4f4
Kristoffcolaso <73115005+Kristoffcolaso@users.noreply.github.com>
2021-02-27 15:01:44 +0530

    Add files via upload

---
+++ project 42/p5.play-boilerplate-master/sketch.js
@@ -0,0 +1,16 @@
+var hour=0;
+var minute=0;
+var second=0;
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(255,255,255);  
+  
+  
+  
+  drawSprites();
+
+}
